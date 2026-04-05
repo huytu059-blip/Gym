dc36a9762289f81d369aa1247f18106b1d1d0a80
duxn9 <tuandungmu07@gmail.com>
2026-04-05 15:40:34 +0700

    Update Registration.h

--- Registration.h
+++ Registration.h
@@ -29,3 +29,59 @@ class Registration {
 
 
 #endif //GYM_REGISTRATION_H
+
+// Phần của Dũng
+//
+// Created by Administrator on 4/5/2026.
+//
+
+#ifndef GYM_REGISTRATION_H
+#define GYM_REGISTRATION_H
+#include <string>
+#include <iostream>
+#include "Package.h"
+
+
+class Registration {
+private:
+    std::string regId;
+    std::string memberId;
+    int packageId;
+    std::string startDate;
+    std::string endDate;
+    double price;
+public:
+    Registration(std::string regid, std::string memId, Package p, std::string start) {
+        this->regId = regid;
+        this->memberId = memId;
+        this->startDate = start;
+        packageId = p.getPackageID();
+        price = p.getPrice();
+        endDate = startDate;
+    }
+    std::string getRegId() const {
+        return this->regId;
+    }
+    std::string getMemberId() const {
+        return this->memberId;
+    }
+    int getPackageId() const {
+        return this->packageId;
+    }
+    std::string getStartDate() const {
+        return this->startDate;
+    }
+    std::string getEndDate() const {
+        return this->endDate;
+    }
+    double getPrice() const {
+        return this->price;
+    }
+    bool isActive(std::string currentDate) const {
+        return currentDate <= endDate;
+    }
+};
+
+
+
+#endif //GYM_REGISTRATION_H
