babb286906ab6d0493c05251783f708a95a4951d
Nem207 <huynem07vn@gmail.com>
2026-04-05 15:04:36 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
-#include "GymController.h"
-
+#include <iostream>
+#include "Controller/GymController.h"
+#include <unordered_map>
 int main() {
-    GymController app;
-    app.run();
+    std::unordered_map<int, Member> Members;
+    std::unordered_map<int, Registration> Reg;
+
+    // Ví dụ thêm dữ liệu
+    Member m1(1, "Huy");
+    Member m2(2, "An");
+
+    Members.insert({m1.getMemberId(), m1});
+
+    Registration r1(1, "2025-01-01", "2025-12-31");
+    Registration r2(2, "2024-01-01", "2024-12-31");
+
+    Reg.insert({r1.getRegID(), r1});
+
+    std::string currentDate = "2025-06-01";
+
+    GymController controller;
+    controller.listActiveMembers(currentDate);
+
     return 0;
 }
