87177721fe77df3d4fd493b6043f58cc888a8cc3
Nem207 <huynem07vn@gmail.com>
2026-04-05 15:03:43 +0700

    Update Package.h

--- Package.h
+++ Package.h
@@ -2,22 +2,54 @@
 // Created by FPT SHOP on 4/3/2026.
 //
 
-#ifndef GYM_PACKAGE_H
-#define GYM_PACKAGE_H
+#ifndef GYM_REGISTRATION_H
+#define GYM_REGISTRATION_H
+#include <string>
 
 
-class Package {
-private:
-    int packageId;
-    string packageName;
-    string time;
-    double packagePrice;
-    public:
-    void registrationPackage();
-    void cancelPackage();
-    void extendPackage();
+class Registration {
+    int registrationId;
+    string registrationDate;
+    int memberId;
+    int PackageId;
+    string dateBegin;
+    string dateEnd;
+    double priceValue;
+    std::string Datecurrent;
+    void displayRegistration();
+    void statisticsQuanitymember(packageId);
+    void seacrhPackageActive(time);
+    void totalPrPackage();
+    void popularyMemberDeactivated();
+    void displaice(time);
+    void autoUpdate();
+
+public:
+    Registration(int ID, std::string Begin, std::string End)
+    :registrationId(ID), dateBegin(Begin), dateEnd(End) {}
+
+    std::string getdateBegin() {
+        return dateBegin;
+    }
+
+    std::string getdateEnd() {
+        return dateEnd;
+    }
+
+    int getMemberId() {
+        return memberId;
+    }
+
+    int getRegID() {
+        return registrationId;
+    }
+
+    std::string getdateCurrent() {
+        return Datecurrent;
+    }
+
 
 };
 
 
-#endif //GYM_PACKAGE_H
+#endif //GYM_REGISTRATION_H
