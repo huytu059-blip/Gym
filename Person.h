62375479146fe6d51d35a2a98d398cd48a768c4a
DtwoDent <huytu059@gmail.com>
2026-04-06 17:39:45 +0700

    Refactor Person class access modifiers and methods
    Changed member variables from private to protected and updated getter methods to be pure virtual.

--- Person.h
+++ Person.h
@@ -1,38 +1,30 @@
-//
-// Created by FPT SHOP on 4/4/2026.
-//
-
 #ifndef GYM_PERSON_H
 #define GYM_PERSON_H
 
+#include <string>
+
 class Person {
-private:
+protected: // 🔥 đổi từ private → protected
     int id;
     std::string name;
-    std::string phoneNumber;
+    std::string numberPhone;
+
 public:
+    Person() {}
 
-    Person() {
-        int id = 0;
-        std::string name = "";
-        std::string phoneNumber = "";
-
-    }
-    Person(int id, std::string name, std::string phoneNumber) {
+    Person(int id, std::string name, std::string numberPhone) {
         this->id = id;
         this->name = name;
-        this->phoneNumber = phoneNumber;
-    };
+        this->numberPhone = numberPhone;
+    }
 
-    virtual int getId() { return id; }
-    virtual std::string getName() { return name; }
-    virtual std::string getPhoneNumber() { return phoneNumber; }
+    // virtual destructor (chuẩn OOP)
+    virtual ~Person() {}
 
-    void setId(int id) { this->id = id; }
-    void setName(std::string name) { this->name = name; }
-    void setPhoneNumber(std::string phoneNumber) { this->phoneNumber = phoneNumber; }
-
+    // getter (pure virtual)
+    virtual int getId() = 0;
+    virtual std::string getName() = 0;
+    virtual std::string getNumberPhone() = 0;
 };
 
-
-#endif //GYM_PERSON_H
+#endif
