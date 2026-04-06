9b9b34bfb78a2f6ec5e4247b9c5301af88e11c4c
DtwoDent <huytu059@gmail.com>
2026-04-06 14:39:21 +0700

    Refactor Person class with private members and constructor

--- Person.h
+++ Person.h
@@ -5,13 +5,21 @@
 #ifndef GYM_PERSON_H
 #define GYM_PERSON_H
 
-
 class Person {
-    Id
-    name
-    numberPhone
-    getter setter
-};
+    private:
+    int id;
+    std::string name;
+    std::string numberPhone;
+    public:
+    Person(int id, std::string name, std::string numberPhone) {
+        this->id = id;
+        this->name = name;
+        this->numberPhone = numberPhone;
+    };
+    virtual void getId() =0;
+    virtual void getName() =0;
+    virtual void getNumberPhone()=0;
 
+};
 
 #endif //GYM_PERSON_H
