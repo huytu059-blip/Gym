6fbc61b163abd18b5c45528ebae6bfe62622549e
Qaggo <yenthinh9b.pvquang@gmail.com>
2026-04-08 12:28:36 +0700

    Update void member.h mới sửa

--- void member.h
+++ void member.h
@@ -12,30 +12,59 @@
 #include "Package.h"
 #include "Registration.h"
 #include "Trainer.h"
+#include "../View/GymView.h"
 
 //member
-class GymManager {
+/*class GymManager :public GymView {
 private:
     std::unordered_map<int, Member> Members;
     std::unordered_map<int,Package> Packages;
-    std::unordered_map<int, Registration> Reg;
+    std::unordered_map<int, Registration> Registations;
     std::unordered_map<int, Trainer> Trainers;
-    public:
-    void Display_all_members
-    (std::unordered_map<int, Member> Members, std::string keyWord);
-    void Search_members
-    (std::unordered_map<int, Member> Members, std::string keyWord) ;
+public:
+        void memberScreen() ;
+        void DisplayAllMembers();
+        void Searchmembers();
+        bool addMember(const Member& m);
+        bool updateMember(int id, const Member& newData);
+        bool deleteMemberById(int id);
+        bool extendPackage(int memberId, int extraDays);
+        const std::unordered_map<int, Member>& DisplayAllmembers() ;
+
+
+};*/
+
+
+#define GYM_GYMMANAGER_H
+
+#include <string>
+#include <unordered_map>
+
+#include "Member.h"
+#include "Package.h"
+#include "Registration.h"
+#include "Trainer.h"
+
+class GymManager {
+private:
+    std::unordered_map<int, Member> Members;
+    std::unordered_map<int, Package> Packages;
+    std::unordered_map<int, Registration> Registations;
+    std::unordered_map<int, Trainer> Trainers;
+
+public:
+    // menu
+    void memberScreen();
+
+    // member functions
+    const std::unordered_map<int, Member>& DisplayAllMembers() const;
+    std::unordered_map<int, Member> searchMembers(const std::string& keyWord) const;
+
     bool addMember(const Member& m);
     bool updateMember(int id, const Member& newData);
     bool deleteMemberById(int id);
-    int deleteMemberByName(const std::string& name);
     bool extendPackage(int memberId, int extraDays);
-    const std::unordered_map<int, Member>& getAllMembers() const;
 };
-
-
-
-
 
 
 
